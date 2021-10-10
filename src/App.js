6393d189a86aac83490af14aa1c99a2377fbf08f
dessolat <desola2@inbox.ru>
2021-10-10 20:51:39 +0300

    Create basic routing, add mui deps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Container, CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Chat from './components/Chat';
 import './App.scss';
 
+
+
 function App() {
-  return <div></div>;
+  return (
+    <Container maxWidth='md'>
+      <CssBaseline />
+			<Router>
+				<Route path="/" exact component={Login} />
+				<Route path="/chat" component={Chat} />
+			</Router>
+    </Container>
+  );
 }
 
 export default App;
