fc630d36c0ac40300b7aac409b0ad50a32cfe5e3
dessolat <desola2@inbox.ru>
2021-10-10 23:52:10 +0300

    Create Login page component, add styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { Container, CssBaseline } from '@mui/material';
+import { Container, CssBaseline, Box } from '@mui/material';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Chat from './components/Chat';
 import './App.scss';
 
-
-
 function App() {
   return (
     <Container maxWidth='md'>
       <CssBaseline />
-			<Router>
-				<Route path="/" exact component={Login} />
-				<Route path="/chat" component={Chat} />
-			</Router>
+      <Box
+        sx={{
+          pt: 20,
+          display: 'flex',
+					justifyContent: 'center'
+        }}>
+        <Router>
+          <Route path='/' exact component={Login} />
+          <Route path='/chat' component={Chat} />
+        </Router>
+      </Box>
     </Container>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Paper, Box, TextField, Button, InputAdornment } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
+import ForumIcon from '@mui/icons-material/Forum';
 
 const Login = () => {
-	return (
-		<div>
-			Login page
-		</div>
-	)
-}
+  const [userName, setUserName] = useState('');
+  const [channel, setChannel] = useState('');
 
-export default Login
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        maxWidth: 250,
+        padding: 3
+      }}>
+      <Box component='form' onSubmit={handleSubmit} noValidate>
+        <TextField
+          required
+          fullWidth
+          name='username'
+					label='Name'
+          autoFocus
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <AccountCircle />
+              </InputAdornment>
+            )
+          }}
+        />
+				
+        <TextField
+          margin='normal'
+          padding='normal'
+          required
+          fullWidth
+          name='channel'
+          label='Channel'
+          type='text'
+					InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <ForumIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+        <Button type='submit' fullWidth variant='contained' sx={{ mt: 3 }}>
+          Join to chat
+        </Button>
+      </Box>
+    </Paper>
+  );
+};
+
+export default Login;
