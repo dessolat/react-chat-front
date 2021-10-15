feb4de6d5e1db309fad70faf3debcc37929d4bc6
Igor Nikiforov <desola2@inbox.ru>
2021-10-15 23:30:42 +0300

    Create snackbar connection notify to Chat comp

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 import { useDispatch, useSelector } from 'react-redux';
-import { Paper, Box, TextField, Stack, Button, Typography } from '@mui/material';
+import { Paper, Box, TextField, Stack, Button, Typography, Slide, Snackbar, Alert } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import { setName } from '../redux/actionCreators';
 import useInput from '../hooks/useInput';
@@ -9,16 +9,18 @@ import useLogging from '../hooks/useLogging';
 
 const Chat = () => {
   const [socket, setSocket] = useState(null);
+  const [connected, setConnected] = useState(false);
+  const [snackbar, setSnackbar] = useState(false);
+  const [messages, setMessages] = useState([]);
   const dispatch = useDispatch();
-  const [input, setInput] = useInput();
   const name = useSelector(state => state.name);
   const channel = useSelector(state => state.channel);
-  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useInput();
   const login = useLogging();
   const messagesEndRef = useRef();
 
   useEffect(() => {
-    setSocket(io('http://192.168.1.4:5000'));
+    setSocket(io('http://192.168.1.24:5000'));
 
     if (!name) {
       const savedName = localStorage.getItem('name');
@@ -35,7 +37,14 @@ const Chat = () => {
     if (socket === null) return;
 
     socket.on('connect', () => {
+      setConnected(true);
+      setSnackbar(true);
       socket.emit('join', { name, channel });
+    });
+
+    socket.on('disconnect', () => {
+      setConnected(false);
+      setSnackbar(true);
     });
 
     socket.on('receive-message', (name, message) => {
@@ -58,48 +67,67 @@ const Chat = () => {
     setInput('');
   };
 
+  function SlideTransition(props) {
+    return <Slide {...props} direction='up' />;
+  }
+
   return (
-    <Box pt={5}>
-      <Paper
-        elevation={3}
-        sx={{
-          height: '80vh',
-          padding: '20px 10px',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          borderRadius: 3,
-          '& .MuiOutlinedInput-root, .MuiButton-root': { borderRadius: 5 },
-          '& .MuiButton-endIcon': { marginLeft: 0 }
-        }}>
-        <Box flexGrow={1} sx={{ padding: 2, overflowY: 'auto' }}>
-          {messages.map((message, i) => (
-            <Stack key={i} direction='row' justifyContent='flex-end' mt={1}>
-              <Stack sx={{ bgcolor: 'primary.main', color: 'white', padding: 1, borderRadius: 5 }}>
-                <Typography
-                  sx={{ fontSize: '1rem' }}>{`${message.name}: ${message.message}`}</Typography>
+    <>
+      <Box pt={5}>
+        <Paper
+          elevation={3}
+          sx={{
+            height: '80vh',
+            padding: '20px 10px 10px 10px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            borderRadius: 3,
+            '& .MuiOutlinedInput-root, .MuiButton-root': { borderRadius: 5 },
+            '& .MuiButton-endIcon': { marginLeft: 0 }
+          }}>
+          <Box flexGrow={1} sx={{ padding: 2, overflowY: 'auto' }}>
+            {messages.map((message, i) => (
+              <Stack key={i} direction='row' justifyContent='flex-end' mt={1}>
+                <Stack sx={{ bgcolor: 'primary.main', color: 'white', padding: 1, borderRadius: 5 }}>
+                  <Typography sx={{ fontSize: '1rem' }}>{`${message.name}: ${message.message}`}</Typography>
+                </Stack>
               </Stack>
-            </Stack>
-          ))}
-          <div ref={messagesEndRef} />
-        </Box>
-        <Stack pt={2} direction='row' spacing={1} component='form' onSubmit={handleSubmit}>
-          <TextField
-            value={input}
-            onChange={setInput}
-            placeholder='Text to send'
-            size='small'
-            sx={{ flexGrow: 1 }}
-          />
-          <Button
-            variant='contained'
-            type='submit'
-            size='small'
-            endIcon={<SendIcon />}
-            sx={{ minWidth: '38px' }}></Button>
-        </Stack>
-      </Paper>
-    </Box>
+            ))}
+            <div ref={messagesEndRef} />
+          </Box>
+          <Stack pt={2} direction='row' spacing={1} component='form' onSubmit={handleSubmit}>
+            <TextField
+              value={input}
+              onChange={setInput}
+              placeholder='Text to send'
+              size='small'
+              sx={{ flexGrow: 1 }}
+              disabled={!connected}
+            />
+            <Button
+              variant='contained'
+              type='submit'
+              size='small'
+              disabled={!connected}
+              endIcon={<SendIcon />}
+              sx={{ minWidth: '38px' }}></Button>
+          </Stack>
+        </Paper>
+      </Box>
+      <Snackbar
+        open={snackbar}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        onClose={() => setSnackbar(false)}
+        TransitionComponent={SlideTransition}>
+        <Alert
+          onClose={() => setSnackbar(false)}
+          severity={connected ? 'success' : 'error'}
+          sx={{ width: '100%' }}>
+          {connected ? `You are succesfully connected to chat as ${name}` : `You are disconnected from chat`}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
