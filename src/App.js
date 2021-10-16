99b47b3e13b23a2d229ac83a82ed2cd272928af3
Igor Nikiforov <desola2@inbox.ru>
2021-10-16 19:33:10 +0300

    Redesign MessageListItem, change cont max-width

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }, []);
 
   return (
-    <Container maxWidth='sm'>
+    <Container maxWidth='md'>
       <CssBaseline />
       <Router>{isLogged ? privateRoutes() : publicRoutes()}</Router>
     </Container>
--- src/components/Chat/MessageListItem.js
+++ src/components/Chat/MessageListItem.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { Stack, Typography } from '@mui/material';
+import { Chip, Stack, Typography } from '@mui/material';
 
 const MessageListItem = ({ message }) => (
   <Stack direction='row' justifyContent='flex-end' mt={1}>
-    <Stack sx={{ bgcolor: 'primary.main', color: 'white', padding: 1, borderRadius: 5 }}>
-      <Typography sx={{ fontSize: '1rem' }}>{`${message.name}: ${message.message}`}</Typography>
+    <Stack
+      direction='column'
+      bgcolor='#ccdaf1'
+      color='black'
+      p={0.8}
+      alignItems='flex-end'
+      sx={{
+        borderRadius: '10px 10px 0 10px',
+        position: 'relative',
+        '&::after': {
+          content: '""',
+          position: 'absolute',
+          bottom: 0,
+          right: '-4px',
+          width: '4px',
+          height: '4px',
+          border: '2px solid transparent',
+          borderBottom: '2px solid #ccdaf1',
+          borderLeft: '2px solid #ccdaf1'
+        }
+      }}>
+      <Chip
+        label={message.name}
+        sx={{
+          bgcolor: '#FFFFFF',
+          color: 'primary.main',
+          fontWeight: 500,
+          fontSize: '0.8rem',
+          height: '1rem',
+          '& .MuiChip-label': { padding: '0 5px' }
+        }}
+      />
+      <Typography mt={0.7} p={0.3} fontSize='.8rem' sx={{ lineHeight: 0.6 }}>
+        {message.message}
+      </Typography>
     </Stack>
   </Stack>
 );
